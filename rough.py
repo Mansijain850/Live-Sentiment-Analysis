8275d201bae407df93cd5014773952fe345d4883
Mansijain850 <mansijain850@gmail.com>
2017-10-13 10:21:43 +0530

    Update rough.py

--- rough.py
+++ rough.py
@@ -2,10 +2,10 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
-ckey = 'TbtXEHu7c7MLF5mLwtlVuzZWs'
-csecret = 'gpJkew4H7GrVOm7mepMI9nQ4j6JTBwZEoGn227lPzp65mrGALZ' 
-atoken = '862342851199328257-EqKAMbIDT3dWiGZp3lfrdTMkMklCVEr'
-asecret = 'cNi6DPGlCHPddHNR9NLDn0eBuT8sxSTnvio6arsYkeSWw'
+ckey = ''
+csecret = '' 
+atoken = ''
+asecret = ''
 
 
 """class Listener(StreamListener):
